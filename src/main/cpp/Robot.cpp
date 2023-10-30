1bc215244e2fb5092239c5a5400309ba60f6c5de
tssmith7 <tssmith7@users.noreply.github.com>
2023-10-29 21:55:08 -0700

    Initial commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+void Robot::RobotInit() {}
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {
+  frc::ChassisSpeeds speeds; 
+
+  speeds.vx = vx_axis.GetAxis() * physical::kMaxDriveSpeed;
+  speeds.vy = vy_axis.GetAxis() * physical::kMaxDriveSpeed;
+  speeds.omega = omega_axis.GetAxis() * physical::kMaxTurnSpeed;
+  m_drive.Drive( speeds );
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+void Robot::SimulationPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ src/main/include/Robot.h
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc/XboxController.h>
+
+#include "ControllerAxis.h"
+#include "SwerveDrive.h"
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+
+  void TestInit() override;
+  void TestPeriodic() override;
+
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+ private:
+  frc::XboxController m_xbox{0};
+  ControllerAxis vx_axis{m_xbox, frc::XboxController::Axis::kLeftY, true};
+  ControllerAxis vy_axis{m_xbox, frc::XboxController::Axis::kLeftX, true};
+  ControllerAxis omega_axis{m_xbox, frc::XboxController::Axis::kRightX, true};
+
+  SwerveDrive m_drive;
+};
